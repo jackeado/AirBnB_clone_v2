c38c084a5a2efce1915123fedc5e2a06199f1035
jackeado <pabsanchez@gmail.com>
2020-04-23 05:02:38 +0000

    starts a Flask web application

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Minimal flask app"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Route index"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def enroute():
+    """Route /hbnb"""
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", 5000)
